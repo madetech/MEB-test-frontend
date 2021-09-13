44431bf664f8865e5a0807a1287bf116f210ef00
el <elena@madetech.com>
2021-09-13 17:17:09 +0100

    read list from api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const url = "https://meb-test-api.azurewebsites.net/weatherforecast"
+  const [fetchData, setFetchData] = useState(null);
+
+    useEffect(() => {
+      getData();
+
+      async function getData() {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setFetchData(data) ;
+      }
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {fetchData && fetchData.map(function(element, index) {
+            return <li key={index}>
+              <ul>
+                <li>{element.date}</li>
+                <li>{element.temperatureC}</li>
+                <li>{element.temperatureF}</li>
+                <li>{element.summary}</li>
+              </ul>
+            </li>
+          })}
+        </ul>
       </header>
     </div>
   );
