614ba647369e83c4f4cfb56be92bf95be699e44d
el <elena@madetech.com>
2021-09-13 15:45:29 +0100

    move all react things to sub folder move function things to root trying to deploy
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
   const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = "hello";
+  const responseMessage = "hello 😻";
 
   context.res = {
     // status: 200, /* Defaults to 200 */
